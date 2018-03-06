2ca74fb12ffa83fe170706f3e212aad5dcca8e6b
aya.murata <aya.murata093@gmail.com>
2018-03-06 07:36:12 +0000

    new product

--- eshop/db/schema.rb
+++ eshop/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305032820) do
+ActiveRecord::Schema.define(version: 20180306044736) do
+
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.integer  "stock"
+    t.text     "note",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
